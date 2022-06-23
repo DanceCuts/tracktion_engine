df87dceb0d66c4e97893d28a8ed9484e9f6193f4
Jordan Skousen <35109863+JordanSkousen@users.noreply.github.com>
2022-06-23 14:19:19 -0600

    No render needed if speed ratio is 1.0!

--- modules/tracktion_engine/model/clips/tracktion_WaveAudioClip.cpp
+++ modules/tracktion_engine/model/clips/tracktion_WaveAudioClip.cpp
@@ -93,7 +93,7 @@ juce::File WaveAudioClip::getOriginalFile() const
 bool WaveAudioClip::needsRender() const
 {
     return ! isUsingMelodyne()
-        && (isReversed || warpTime || (clipEffects != nullptr && canHaveEffects()))
+        && (isReversed || (warpTime && speedRatio != 1.0) || (clipEffects != nullptr && canHaveEffects()))
         && AudioFile (edit.engine, getOriginalFile()).isValid();
 }
 
